f2fb06d348c21392b5ae32c300f8ac4ffd4300b2
Steve Woodis <swoodis2010@gmail.com>
2017-04-10 19:19:06 -0600

    images are coming thru. Next get them side by side.

--- src/components/monsterMoviesComponent/moviesList.js
+++ src/components/monsterMoviesComponent/moviesList.js
@@ -7,7 +7,7 @@ class monsterData extends React.Component{
     this.state = {items: []}
   }
   componentWillMount(){
-    fetch('https://api.themoviedb.org/3/search/movie?api_key=2c8889cb44ec3da352062419180957cf&language=en-US&query=fairy&page=1&include_adult=false&region=fairy&year=>1960')
+    fetch('https://api.themoviedb.org/3/search/movie?api_key=2c8889cb44ec3da352062419180957cf&language=en-US&query=vampire&page=1&include_adult=false&year=>1960')
       .then(response => response.json())
       .then( ({results: items}) => this.setState({items}))
   }
@@ -19,7 +19,7 @@ class monsterData extends React.Component{
       <div className="main">
         {items.map(item =>
           <div key={item.id} className="movieBox">
-             <div className="backdropPic"><img src={imageBaseUrl + item.backdrop_path}/></div>
+             <div className="backdropPic"><img src={imageBaseUrl + item.poster_path} alt="poster"/></div>
               <div className="info">
                 <div className="movieTitle">{item.title}</div>
                 <div className="movieOverview">{item.overview}</div>
